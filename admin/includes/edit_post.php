fd703700dfb82ff2b97eb12f609220ecded3573c
francgoz99 <okoligozie972@gmail.com>
2022-04-07 00:26:57 +0100

    init

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -175,10 +175,31 @@ if(isset($_POST['update_post'])) {
           <input value="<?php echo $post_author; ?>" type="text" class="form-control" name="post_author">
       </div>
 
-    <div class="form-group">
+     <div class="form-group">
+    <select name="post_status" id="">
+    <option value='<?php echo $post_status; ?>'><?php echo $post_status; ?></option>
+        <?php if ($post_status == 'published'){
+            echo "<option value='draft'>Draft</option>";
+
+        }else{
+            echo "echo <option value='published'>Publish</option>";
+
+        }
+
+            
+            ?>
+
+    </select>
+     </div>
+
+
+
+
+
+   <!-- <div class="form-group">
         <label for="post_status">Post Status</label>
         <input value="<?php echo $post_status; ?>" type="text" class="form-control" name="post_status">
-    </div>
+    </div> -->
 
 
 
